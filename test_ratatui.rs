fa72a7ff1130ce56dbb2490e719c1236e7ccab0f
Rathan <rithikrathan.c@gmail.com>
2026-08-29 22:37:58 +0530

    Remove test files

--- test_ratatui.rs
+++
@@ -1,7 +0,0 @@
-use ratatui::buffer::Buffer;
-use ratatui::layout::Rect;
-fn test(buf: &mut Buffer) {
-    let cell = buf.get_mut(0, 0);
-    cell.set_symbol("█");
-    cell.set_fg(ratatui::style::Color::Red);
-}
